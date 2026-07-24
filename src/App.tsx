5ec50ad0de7f97c36856c9a8dcdbe53e054cb90e
Aboubakary Traore <abou@Aboubakarys-MacBook-Air.local>
2026-07-23 20:58:37 -0500

    learned to use fetch in react.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import Navbar from "./components/Navbar"
+import { useEffect, useState } from 'react';
 export default function App(){
-  fetch("https://localhost:4000/users")
-  .then(res=>res.json())
-  .then(data =>console.log(data))
+//Use Effect(function, [if this happens the function will run.])
+  const [users , setUsers] = useState([]);
+
+  useEffect(()=> {fetch("http://localhost:4000/api/users", {
+    method: 'GET',
+    headers: {
+      'Content-Type' : 'application/json'
+    },})
+  .then(res=>{
+    if (res.ok){
+      return res.json();
+    }
+    else{
+      console.log("not successful")
+    }
+  })
+  .then(json =>setUsers(json))
+}
+)
+
   return(
     
     <div>
       <div>
+        <Navbar/>
         <h1>Dashboard</h1>
-        {console.log()}
+        {users.map(user =>{return <pre>{JSON.stringify(user)}</pre>})}
       </div>
     </div>
   )
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar(){
+    return(
+        <div className = "flex border-b w-full">
+        <h1>MESP</h1>
+        </div>
+    )
+
+}
